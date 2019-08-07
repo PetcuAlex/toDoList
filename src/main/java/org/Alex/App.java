07ad2b63c7a04741043b8db0a488ff640b2fe826
Alex <adilex99@yahoo.com>
2019-08-07 20:13:25 +0300

    added repository methods for CRUD operations

--- src/main/java/org/Alex/App.java
+++ src/main/java/org/Alex/App.java
@@ -1,13 +1,22 @@
 package org.Alex;
 
+import org.Alex.persistence.ToDoItemRepository;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
+        ToDoItemRepository toDoItemRepository = new ToDoItemRepository();
+//        toDoItemRepository.createToDoItem("Learn Dancing", LocalDateTime.now().plusMonths(14));
+
+//        toDoItemRepository.deleteToDoItem(4);
+        toDoItemRepository.updateToDoItem(3,true);
+
+        System.out.println(toDoItemRepository.getToDoItems());
     }
 }
--- src/main/java/org/Alex/persistence/ToDoItemRepository.java
+++ src/main/java/org/Alex/persistence/ToDoItemRepository.java
@@ -1,10 +1,12 @@
 package org.Alex.persistence;
 
+import org.Alex.domain.ToDoItem;
+
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ToDoItemRepository {
 
@@ -12,15 +14,66 @@ public class ToDoItemRepository {
         String insertSql = "INSERT INTO to_do_item (description, deadline) VALUES (?, ?)";
 
         //try-with-resources
-        try(Connection connection = DatabaseConfiguration.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(insertSql)
-        ){
-            preparedStatement.setString(1,description);
-            preparedStatement.setDate(2,java.sql.Date.valueOf(deadline.toLocalDate()));
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(insertSql)
+        ) {
+            preparedStatement.setString(1, description);
+            preparedStatement.setDate(2, java.sql.Date.valueOf(deadline.toLocalDate()));
 
             preparedStatement.executeUpdate();
         }
 
 
+    }
+
+    public List<ToDoItem> getToDoItems() throws SQLException, IOException, ClassNotFoundException {
+        String query = "SELECT id,description,deadline,status FROM to_do_item";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             Statement statement = connection.createStatement();
+        ) {
+            ResultSet resultSet = statement.executeQuery(query);
+
+            List<ToDoItem> toDoItems = new ArrayList<>();
+
+            while (resultSet.next()) {
+                ToDoItem item = new ToDoItem();
+                item.setId(resultSet.getLong("id"));
+                item.setDescription(resultSet.getString("description"));
+                item.setDeadline(resultSet.getDate("deadline").toLocalDate().atStartOfDay());
+                item.setStatus(resultSet.getBoolean("status"));
+
+                toDoItems.add(item);
+
+            }
+            return toDoItems;
+        }
+
+    }
+
+    public void deleteToDoItem(long id) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "DELETE FROM to_do_item WHERE id = ?";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            preparedStatement.setLong(1, id);
+
+            preparedStatement.executeUpdate();
+        }
+
+
+    }
+
+    public void updateToDoItem(long id,boolean status) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "UPDATE to_do_item SET status = ? WHERE id = ?";
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ){
+            preparedStatement.setBoolean(1,status);
+            preparedStatement.setLong(2,id);
+
+            preparedStatement.executeUpdate();
+        }
     }
 }
